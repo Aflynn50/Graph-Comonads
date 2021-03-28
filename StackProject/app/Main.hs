a07b3cc42454dbb4d3ce2981ca4bd58df79a3f14
Alastair Flynn <alastairflynn@hotmail.co.uk>
2021-03-28 19:24:19 +0100

    Not sure this is right but saving anyway

--- StackProject/app/Main.hs
+++ StackProject/app/Main.hs
@@ -4,4 +4,4 @@ import AlgGraphs
 
 main :: IO ()
 --main = return ()
-main = (putStr . show) res2
+main = (putStr . show) res1
--- StackProject/src/AlgGraphs.hs
+++ StackProject/src/AlgGraphs.hs
@@ -14,7 +14,7 @@ module AlgGraphs where
 
 import Algebra.Graph.Class
 import Algebra.Graph.AdjacencyMap as AdjMap
-import Algebra.Graph.NonEmpty.AdjacencyMap (fromNonEmpty)
+import Algebra.Graph.NonEmpty.AdjacencyMap (fromNonEmpty, overlay)
 import Algebra.Graph.AdjacencyMap.Algorithm
 
 
@@ -51,9 +51,14 @@ class Graph g => GraphExtras g where
 
 -- one of these two defns
 -- data EF a where EFdata :: Graph [a] => a -> EFdata a
-data EF a where EFdata :: (Graph a, Ord a) => AdjacencyMap [Vertex a] -> EF a
+data EF a where EFdata :: (Graph a) => AdjacencyMap [Vertex a] -> EF a
 
-deriving instance (Graph a, Ord a, Show a, Ord (Vertex a), Show (Vertex a)) => Show (EF a)
+deriving instance (Graph a, Ord a, Show a, Ord (Vertex a), Show (Vertex a)) => Show (EF a)    
+
+-- We need the Ord here because the Graph instance for AdjMaps needs it
+instance Ord a => GraphExtras (AdjacencyMap a) where
+    getEdges = edgeList
+    getVertices = vertexList
 
 instance (Graph a, Ord a, Ord (Vertex a)) => Graph (EF a) where
     type Vertex (EF a) = [Vertex a]
@@ -62,10 +67,9 @@ instance (Graph a, Ord a, Ord (Vertex a)) => Graph (EF a) where
     overlay (EFdata g1) (EFdata g2) = EFdata $ AdjMap.overlay g1 g2
     connect (EFdata g1) (EFdata g2) = EFdata $ AdjMap.connect g1 g2
 
-instance GraphExtras (EF a) where
+instance (Graph a, Ord a, Ord (Vertex a)) => GraphExtras (EF a) where
     getEdges (EFdata g) = edgeList g
     getVertices (EFdata g) = vertexList g
-
 
 instance CFunctor EF GraphMorphism GraphMorphism where
      funcMap (GM f) = GM (map f)
@@ -76,36 +80,34 @@ instance CComonad EF GraphMorphism where
 
 data Product a b where Prod :: (Graph a, Graph b) => AdjacencyMap (Vertex a, Vertex b) -> Product a b
 
-instance (Graph a, Graph b) => Graph (Product a b) where
+instance (Graph a, Graph b, Ord (Vertex a), Ord (Vertex b)) => Graph (Product a b) where
     type Vertex (Product a b) = (Vertex a, Vertex b)
     empty = Prod AdjMap.empty
     vertex v = Prod $ AdjMap.vertex v
     overlay (Prod g1) (Prod g2) = Prod $ AdjMap.overlay g1 g2
     connect (Prod g1) (Prod g2) = Prod $ AdjMap.connect g1 g2
 
-instance GraphExtras (Product a b) where
+instance (Graph a, Graph b, Ord (Vertex a), Ord (Vertex b)) => GraphExtras (Product a b) where
     getEdges (Prod g) = edgeList g
     getVertices (Prod g) = vertexList g
 
 instance CBifunctor Product GraphMorphism GraphMorphism GraphMorphism where
   bifuncMap (GM gm1) (GM gm2) = GM (\(x,y) -> (gm1 x, gm2 y))
 
--- finish defen
-product :: AdjacencyMap a -> AdjacencyMap b -> Product (AdjacencyMap a) (AdjacencyMap b)
+product :: (Ord a, Ord b) => AdjacencyMap a -> AdjacencyMap b -> Product (AdjacencyMap a) (AdjacencyMap b)
 product g1 g2 = Prod $ box g1 g2
 
 
--- finish coproduct implementation
 data Coproduct a b where Coprod :: (Graph a, Graph a) => AdjacencyMap (Either (Vertex a) (Vertex b)) -> Coproduct a b
 
-instance (Graph a, Graph b) => Graph (Coproduct a b) where
+instance (Graph a, Graph b, Ord (Vertex a), Ord (Vertex b)) => Graph (Coproduct a b) where
     type Vertex (Coproduct a b) = Either (Vertex a) (Vertex b)
     empty = Coprod AdjMap.empty
     vertex v = Coprod $ AdjMap.vertex v
     overlay (Coprod g1) (Coprod g2) = Coprod $ AdjMap.overlay g1 g2
     connect (Coprod g1) (Coprod g2) = Coprod $ AdjMap.connect g1 g2
 
-instance GraphExtras (Coproduct a b) where
+instance (Graph a, Graph b, Ord (Vertex a), Ord (Vertex b)) => GraphExtras (Coproduct a b) where
     getEdges (Coprod g) = edgeList g
     getVertices (Coprod g) = vertexList g
 
@@ -114,45 +116,59 @@ instance CBifunctor Coproduct GraphMorphism GraphMorphism GraphMorphism where
     where g (Left x)  = Left $ gm1 x
           g (Right x) = Right $ gm2 x
 
-coproduct :: AdjacencyMap a -> AdjacencyMap b -> Coproduct (AdjacencyMap a) (AdjacencyMap b)
+coproduct :: (Ord a, Ord b) => AdjacencyMap a -> AdjacencyMap b -> Coproduct (AdjacencyMap a) (AdjacencyMap b)
 coproduct g1 g2 = Coprod $ AdjMap.connect (gmap Left g1) (gmap Right g2)
 
 ------------------ Example with two equivilent graphs  -------------------- 
+-- With EF comonad I can only prove them equivilent in fragment of logic with quantifier rank k.
+-- To do this we give a coklislie morphism that takes the EK graph with play length k to graph b
+graph1 :: AdjacencyMap Int
+graph1 = AdjMap.edges [(1,2),(2,3)]
 
--- graph1 :: AdjacencyMap Int
--- graph1 = AdjMap.edges [(1,2),(2,3)]
+graph2 :: AdjacencyMap Char
+graph2 = AdjMap.edges [('a','b'),('b','c')]
 
--- graph2 :: AdjacencyMap Char
--- graph2 = AdjMap.edges [('a','b'),('b','c')]
-
--- -- universe of a graph
--- universe :: (GraphExtras a, Eq (Vertex a)) => a -> [Vertex a]
--- universe = nub Prelude.. getVertices
+-- universe of a graph
+universe :: (GraphExtras a, Eq (Vertex a)) => a -> [Vertex a]
+universe = nub Prelude.. getVertices
 
 
--- --given a universe gives all possbile plays for a k length game
--- plays :: Int -> [a] -> [[a]]
--- plays 1 uni = map (\x -> [x]) uni
--- plays k uni = ps ++ [(x:p)|x<-uni,p<-ps]
---     where ps = plays (k-1) uni
+-- Code to generate all compatible plays with a graph with universe uni.
+-- The idea is to generate all k length permutations where every elem of uni occours at least once
+-- Not very nice/efficient code but appears to work
+-- suffixes gets all possible ascending orderings of k-len(uni) elems of the universe
+-- Pre: k > length uni
+plays :: Eq a => Int -> [a] -> [[a]]
+plays k uni = nub $ concatMap permutations (map (uni++) suffixes)
+    where suffixes = map tail (f r uni)
+          r = k - (length uni) + 1
+          f 1 uni = map (\x -> [x]) uni
+          f i uni = nub $ concat [map ((head uni):) ps|ps <- 
+            (map (f (i-1)) ((init Prelude.. tails) uni))]
 
--- -- Given a graph map each vertex to every play that is appears in and generate edges accordingly
--- -- Usually gives a massive graph
--- graphToMaxEFk :: (GraphExtras a, Eq (Vertex a)) => Int -> a -> EF a
--- graphToMaxEFk k g = EFdata $ concatMap (\(x,y)->[(f x p1,f y p2)|p1<-ps,p2<-ps,elem x p1,elem y p2]) (getEdges g)
---     where ps       = plays k (universe g)
---           f y (x:xs)
---             | x==y      = x:xs
---             | otherwise = x:(f y xs)
+--isPlayCompatible :: (GraphExtras a, Eq (Vertex a)) => a -> [Vertex a] -> Bool
+--isPlayCompatible g p = foldr (\x b -> (elem x p) && b) True (getVertices g)
 
--- -- Given a graph map each vertex to every play that is appears in and generate edges accordingly
--- -- Usually gives a massive graph
--- graphToReasonableEFk :: (GraphExtras a, Eq (Vertex a)) => Int -> a -> EF a
--- graphToReasonableEFk k g = EFdata $ concatMap (\(x,y)->take 5 [(f x p1,f y p2)|p1<-ps,p2<-ps,elem x p1,elem y p2]) (getEdges g)
---     where ps       = plays k (universe g)
---           f y (x:xs)
---             | x==y      = x:xs
---             | otherwise = x:(f y xs)
+-- Performs the action of the EFk functor
+graphToEFk :: (GraphExtras a, Eq (Vertex a), Ord (Vertex a)) => Int -> a -> EF a
+graphToEFk k g = EFdata $ AdjMap.edges $
+    concatMap (\p -> map (\(a,b) -> (f p a, f p b)) edgesOfg) ps
+    where edgesOfg = getEdges g
+          ps       = plays k (universe g)
+          f (x:xs) y  -- get prefix of play ending in y
+            | x==y      = [x]
+            | otherwise = x:(f xs y)
+
+-- graphToEFk usually gives a massive graph so this allows a smaller version of it
+graphToLimEFk :: (GraphExtras a, Eq (Vertex a), Ord (Vertex a)) => Int -> Int -> a -> EF a
+graphToLimEFk lim k g = EFdata $ AdjMap.edges $
+    concat $ take lim $ map (\p -> map (\(x,y) -> (f p x, f p y)) edgesOfg) ps
+    where edgesOfg = getEdges g
+          ps       = plays k (universe g)
+          f (x:xs) y  -- get prefix of play ending in y
+            | x==y      = [x]
+            | otherwise = x:(f xs y)
+
 
 -- This doesnt really make sense as a thing. The EF comonad should probably map each vertex
 -- to all plays ending in that vertex, and the edges acordingly to that. Then when the 
@@ -162,7 +178,7 @@ coproduct g1 g2 = Coprod $ AdjMap.connect (gmap Left g1) (gmap Right g2)
 -- It actually does make sense. A coalgebra (which is what this is) needs to map to a prefix closed
 -- subset of A^(>=k). I.e. some particular play and its prefixes. This is given by the coalgebra
 -- conditions.  If it doesn't preserve all edges its invalid becuase its not a homomorphism
--- Look at page 23 or struct and powerAce23578ac
+-- Look at page 23 or struct and power
 -- graphToEFk :: (Graph a, Eq (Vertex a)) => [Vertex a] -> a -> EF a
     
 -- Given a list of plays for a coalgebra 
@@ -192,107 +208,127 @@ coproduct g1 g2 = Coprod $ AdjMap.connect (gmap Left g1) (gmap Right g2)
 
 
 
--- liftMappingToEFMorphism ::(Graph a, Graph b) => (Vertex a -> Vertex b) -> GraphMorphism (EF a) b
--- liftMappingToEFMorphism f = GM f Category.. counit
+liftMapToEFMorph ::(Graph a, Graph b, Ord (Vertex a), Ord a) => (Vertex a -> Vertex b) -> GraphMorphism (EF a) b
+liftMapToEFMorph f = GM f Category.. counit
 
--- myhomomorph :: GraphMorphism (EF (MyGraph Int)) (MyGraph Char) 
--- myhomomorph = liftMappingToEFMorphism f 
---   where f 1 = 'a'
---         f 2 = 'b'
---         f 3 = 'c'
+g1tog2 = liftMapToEFMorph f 
+  where f 1 = 'a'
+        f 2 = 'b'
+        f 3 = 'c'
 
--- apply :: (Graph a, Graph b, Eq (Vertex b)) => GraphMorphism a b -> a -> b
--- apply (GM gm) = gmap gm
+g2tog1 = liftMapToEFMorph f 
+  where f 'a' = 1
+        f 'b' = 2
+        f 'c' = 3 
 
---res1 = apply myhomomorph (head (liftGraphToEFkGraphs 1 graph1)) == graph2
+apply :: (Eq b, Ord a, Ord b) => GraphMorphism (EF (AdjacencyMap a)) (AdjacencyMap b) -> (EF (AdjacencyMap a)) -> AdjacencyMap b
+apply (GM gm) (EFdata g)= gmap gm g
 
--- ------------------ Example with linear orderings  -------------------- 
--- -- Given a list representing a linear ordering it returns the equivilent
--- -- graph with the edge relation interpreted as the < relation
--- linToGraph :: [a] -> MyGraph a
--- linToGraph xs = MyGraph (f xs)
---   where f [] = []
---         f (x:xs) = map (\y -> (x,y)) xs ++ f xs
+-- Checks that there is a valid homomorphism from EF A -> B and EF B -> A. This is the condition
+-- for equality up to quantifier rank k
+eqUpToQuantRankK :: (Eq b, Ord a, Ord b) => Int -> GraphMorphism (EF (AdjacencyMap a)) (AdjacencyMap b) -> 
+    GraphMorphism (EF (AdjacencyMap b)) (AdjacencyMap a) -> AdjacencyMap a -> AdjacencyMap b -> Bool
+eqUpToQuantRankK k h1 h2 g1 g2 = (apply h1 (graphToEFk k g1) == g2) && (apply h2 (graphToEFk k g2) == g1)
 
--- count :: Eq a => a -> [a] -> Int
--- count x = foldr (\y xs-> if x==y then xs + 1 else xs) 0
+res1 = eqUpToQuantRankK 4 g1tog2 g2tog1 graph1 graph2
 
--- -- converts a graph to a linear order
--- -- Pre: Graph is a representation of a lin order
--- graphToLin :: (Graph a, Eq (Vertex a)) => a -> [Vertex a]
--- graphToLin g = reverse $ map fst $ sortBy (\(_,a) (_,b) -> compare a b) $ map (\x -> (x,count x e)) (universe g)
---     where e = map fst (edges g)
+------------------ Example with linear orderings  -------------------- 
+-- Given a list representing a linear ordering it returns the equivilent
+-- graph with the edge relation interpreted as the < relation
+linToGraph :: Ord a => [a] -> AdjacencyMap a
+linToGraph xs = AdjMap.edges (f xs)
+  where f [] = []
+        f (x:xs) = map (\y -> (x,y)) xs ++ f xs
 
--- -- Pre: elem x xs
--- split :: Eq a => a -> [a] -> ([a],[a])
--- split x xs = f [] xs
---     where f xs (y:ys) = if x == y then (reverse (x:xs), x:ys) else f (y:xs) ys
+count :: Eq a => a -> [a] -> Int
+count x = foldr (\y xs-> if x==y then xs + 1 else xs) 0
+
+-- converts a graph to a linear order
+-- Pre: Graph is a representation of a lin order
+graphToLin :: (Graph a, GraphExtras a, Eq (Vertex a)) => a -> [Vertex a]
+graphToLin g = reverse $ map fst $ sortBy (\(_,a) (_,b) -> compare a b) $ map (\x -> (x,count x e)) (universe g)
+    where e = map fst (getEdges g)
+
+-- Pre: elem x xs
+split :: Eq a => a -> [a] -> ([a],[a])
+split x xs = f [] xs
+    where f xs (y:ys) = if x == y then (reverse (x:xs), x:ys) else f (y:xs) ys
           
--- -- Get pos of y in xs
--- -- Pre: y is in xs
--- index y xs = f xs 0
---     where f (x:xs) i
---             | x == y    = i
---             | otherwise = f xs (i+1)
+-- Get pos of y in xs
+-- Pre: y is in xs
+index y xs = f xs 0
+    where f (x:xs) i
+            | x == y    = i
+            | otherwise = f xs (i+1)
 
--- -- Pre: x is in the iso
--- getIso :: Eq a => [(a,b)] -> a -> b
--- getIso ((a,b):iso) x 
---     | x == a     = b
---     | otherwise  = getIso iso x
+-- Pre: x is in the iso
+getIso :: Eq a => [(a,b)] -> a -> b
+getIso ((a,b):iso) x 
+    | x == a     = b
+    | otherwise  = getIso iso x
 
--- -- Given a spoilers play come up with the duplicators 
--- -- This uses the strategy given on page 29 of Libkin
--- -- Pre: length lin1,lin2 >= 2^k
--- --      length spoil <= k
--- strategy :: (Graph a, Graph b, Eq (Vertex a), Eq (Vertex b)) => Int -> a -> b -> [Vertex a] -> [Vertex b]
--- strategy k glin1 glin2 spoil = map (getIso iso) spoil
---     where lin1 = graphToLin glin1
---           lin2 = graphToLin glin2
---           iso  = f k lin1 lin2 spoil
+-- Given a spoilers play come up with the duplicators 
+-- This uses the strategy given on page 29 of Libkin
+-- Pre: length lin1,lin2 >= 2^k
+--      length spoil <= k
+strategy :: (Graph a, Graph b, GraphExtras a, GraphExtras b, Eq (Vertex a), Eq (Vertex b)) 
+                        => Int -> a -> b -> [Vertex a] -> [Vertex b]
+strategy k glin1 glin2 spoil = map (getIso iso) spoil
+    where lin1 = graphToLin glin1
+          lin2 = graphToLin glin2
+          iso  = f k lin1 lin2 spoil
 
--- inIso :: Eq a => a -> [(a,b)] -> Bool
--- inIso x []    = False
--- inIso x ((a,b):ps)
---     | x==a      = True 
---     | otherwise = inIso x ps
+inIso :: Eq a => a -> [(a,b)] -> Bool
+inIso x []    = False
+inIso x ((a,b):ps)
+    | x==a      = True 
+    | otherwise = inIso x ps
 
--- f :: Eq a => Int -> [a] -> [b] -> [a] -> [(a,b)]
--- f _ _ _ [] = []
--- f 0 _ _ _  = []
--- f k lin1 lin2 (p:ps)
---     | inIso p iso = iso
---     | otherwise   = (p,chooseb k lin1 lin2 p) : iso
---       where iso   = f (k-1) lin1 lin2 ps
-
-
--- -- This is the function that given an 'a' in lin1 finds a 'b' in lin2 s.t. lin1 - a =_(k-1) lin2 - b
--- chooseb :: Eq a => Int -> [a] -> [b] -> a -> b
--- chooseb k lin1 lin2 a
---     | length lin1LT < 2^(k-1) = lin2 !! index a lin1
---     | length lin1GT < 2^(k-1) = lin2 !! index a lin1
---     | otherwise               = lin2 !! (2^(k-1))
---     where (lin1LT,lin1GT) = split a lin1
+f :: Eq a => Int -> [a] -> [b] -> [a] -> [(a,b)]
+f _ _ _ [] = []
+f 0 _ _ _  = []
+f k lin1 lin2 (p:ps)
+    | inIso p iso = iso
+    | otherwise   = (p,chooseb k lin1 lin2 p) : iso
+      where iso   = f (k-1) lin1 lin2 ps
 
 
+-- This is the function that given an 'a' in lin1 finds a 'b' in lin2 s.t. lin1 - a =_(k-1) lin2 - b
+chooseb :: Eq a => Int -> [a] -> [b] -> a -> b
+chooseb k lin1 lin2 a
+    | length lin1LT < 2^(k-1) = lin2 !! index a lin1
+    | length lin1GT < 2^(k-1) = lin2 !! index a lin1
+    | otherwise               = lin2 !! (2^(k-1))
+    where (lin1LT,lin1GT) = split a lin1
 
--- buildMorphEFkAtoB :: (Graph a, Graph b, Eq (Vertex a), Eq (Vertex b)) => Int -> a -> b -> GraphMorphism (EF a) b
--- buildMorphEFkAtoB k glin1 glin2 = GM (last Prelude.. strategy k glin1 glin2)
 
--- checkEFkMorph :: (Graph a, Graph b, Eq (Vertex b), Eq (Vertex a)) => Int -> a -> b -> b
+
+buildMorphEFkAtoB :: (Graph a, Graph b, GraphExtras a, GraphExtras b, Eq (Vertex a), Eq (Vertex b), Ord (Vertex a), Ord a)
+                => Int -> a -> b -> GraphMorphism (EF a) b
+buildMorphEFkAtoB k glin1 glin2 = GM (last Prelude.. strategy k glin1 glin2)
+
+
+-- checkEFkMorph :: (GraphExtras a, Graph a, Graph b, Eq (Vertex b), Eq (Vertex a)) => Int -> a -> b -> b
 -- checkEFkMorph k glin1 glin2 = apply (buildMorphEFkAtoB k glin1 glin2) eflin1
---     where eflin1 = head $ genCoalgs k glin1
+--     where eflin1 = graphToEFk k glin1
 
--- lin1 = linToGraph [1..17]
--- lin2 = linToGraph [3..22]
 
--- lin3 = linToGraph [1,2,3]
+checkEFkMorph :: (Ord a, Ord b) => Int -> AdjacencyMap a -> AdjacencyMap b -> AdjacencyMap b
+checkEFkMorph k glin1 glin2 = apply (buildMorphEFkAtoB k glin1 glin2) eflin1
+    where eflin1 = graphToEFk k glin1
 
--- lin4 = linToGraph [1..9]
--- lin5 = linToGraph [4..12]
 
--- lin6 = linToGraph [1..4]
--- lin7 = linToGraph [5..8]
+lin1 :: AdjacencyMap Integer
+lin1 = linToGraph [1..17]
+lin2 = linToGraph [3..22]
+
+lin3 = linToGraph [1,2,3]
+
+lin4 = linToGraph [1..9]
+lin5 = linToGraph [4..12]
+
+lin6 = linToGraph [1..4]
+lin7 = linToGraph [5..8]
 
 ------------------ Tree depth experiments  -------------------- 
 
